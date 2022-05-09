74763142d6b38b38a89fbde42b08b74ee783e5f2
Teloschet <chrisaleiro@gmail.com>
2022-05-08 21:41:02 -0300

    feat: create function widget with boolean array

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,13 +3,17 @@ import { useState } from 'react';
 
 export function Widget() {
 
-  const [] = useState(false)
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen)
+  }
 
   return (
     <div className='absolute bottom-5 right-5'>
-      <p>Hello World</p>
+      { isWidgetOpen && <p>Hello World</p> }
 
-      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
