4c54c1bd27ab43507fd6fa3fe2e53b6653eed73a
Teloschet <chrisaleiro@gmail.com>
2022-05-14 16:34:26 -0300

    feat: create interface props and trash icon function

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,9 +1,14 @@
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from 'html2canvas';
 import { useState } from "react";
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
   const [isTakingScreenshot, setisTakingScreenshot] = useState(false)
 
   async function handleTakeScreenshot() {
@@ -12,9 +17,25 @@ export function ScreenshotButton() {
     const canvas = await html2canvas(document.querySelector('html')!);
     const base64image = canvas.toDataURL('image/png');
 
-    // console.log(base64image)
-
+    onScreenshotTook(base64image);
     setisTakingScreenshot(false);
+  }
+
+  if (screenshot) {
+    return(
+      <button
+        type="button"
+        className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        onClick={() => onScreenshotTook(null)}
+        style={{
+          backgroundImage: `url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize: 180,
+        }}
+      >
+        <Trash weight="fill" />
+      </button>
+    )
   }
 
   return(
