346485604aab7696ccf77e4ae45a00b7a5b831f4
Teloschet <chrisaleiro@gmail.com>
2022-05-14 16:04:21 -0300

    feat!: create state for takingScreenshot and set this boolean

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,12 +1,15 @@
 import { Camera } from "phosphor-react";
 import html2canvas from 'html2canvas';
+import { useState } from "react";
 
 export function ScreenshotButton() {
+  const [isTakingScreenshot, setisTakingScreenshot] = useState(false)
+
   async function handleTakeScreenshot() {
     const canvas = await html2canvas(document.querySelector('html')!);
     const base64image = canvas.toDataURL('image/png');
 
-    console.log(base64image)
+    // console.log(base64image)
   }
 
   return(
